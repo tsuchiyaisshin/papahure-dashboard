1e32ff58bf1c4bb5e839077906e37b6e7e46c3b5
土屋一心 <tsuchiya.isshin@win-techno.com>
2020-09-13 21:49:23 +0900

    create Register.vue

---
+++ src/components/molcules/AppBar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="background-svg">
+        <v-container>
+            <v-row dense class="mt-n5">
+                <v-col cols="9">
+                    <div style="color: white; font-size: 13px; font-weight: 600;" v-text="date">
+                    </div>
+                </v-col>
+                <v-col class="text-right" cols="3">
+                    <img src="../../assets/svg/icons.svg" class="logo">
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="12">
+                    <div style="color: white; font-size: 24px; font-weight: 600; text-align: center" v-text="text">
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppBar",
+        props: {
+            text: {
+                type: String,
+                default: '目標を決めよう！',
+            }
+        },
+        data: () => ({
+            date: undefined
+        }),
+        mounted() {
+            setInterval(this.showClock1)
+        },
+        methods: {
+            showClock1() {
+                const nowTime = new Date();
+                const nowHour = nowTime.getHours();
+                const nowMin = nowTime.getMinutes();
+                const msg = nowHour + ":" + nowMin;
+                this.date = msg
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .background-svg {
+        background-image: url('../../assets/svg/header.svg');
+        max-width: 320px;
+        min-height: 149px;
+    }
+</style>
--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -1,14 +1,38 @@
 <template>
     <div class="background-svg">
+        <app-bar text="目標を決めよう！"></app-bar>
+        <v-container>
+            <v-card>
+                <v-card-title class="select-text">
+                    パパ
+                </v-card-title>
+            </v-card>
+            <v-card style="margin-top: 50px">
+                <v-card-title class="select-text">
+                    プロデューサー
+                </v-card-title>
+            </v-card>
+        </v-container>
     </div>
 </template>
 
 <script>
+    import AppBar from "../molcules/AppBar";
+
     export default {
-        name: "Register"
+        name: "Register",
+        components: {AppBar},
     }
 </script>
 
 <style scoped>
-
+    .select-text {
+        font-weight: 600;
+        font-size: 32px;
+        text-align: center;
+        margin-top: 50px;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        color: orangered
+    }
 </style>
